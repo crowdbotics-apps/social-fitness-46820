7144196cb63fabc7a20b6ccef5dbadc6dcff89a6
Crowdbotics <team@crowdbotics.com>
2024-03-05 16:23:28 +0000

    Initial commit

---
+++ backend/social_fitness_46820/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for social_fitness_46820 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "social_fitness_46820.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "social-fitness-46820",
+  url: "https://social-fitness-46820.botics.co",
+  api: "https://social-fitness-46820.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
